62c0779ff6d54b5c4ae7e048ad2d2cfbcc615853
Samit <Samit.prsl@gmail.com>
2023-04-04 12:39:21 +0530

    Final Patches

--- src/index.js
+++ src/index.js
@@ -27,6 +27,39 @@ catch(e){
 }
 })
 
+app.get('/:id', async (req,res)=>{
+    try{
+        const _id = req.params.id
+        const GetDataId = await SongColl.findByIdAndUpdate({_id});
+        res.status(200).send(GetDataId);
+    }
+catch(e){
+    res.status(400).send(e);
+}
+})
+
+app.patch('/:id', async (req,res)=>{
+    try{
+        const _id = req.params.id
+        const GetDataId = await SongColl.findByIdAndUpdate(_id,req.body,{new:true});
+        res.status(200).send(GetDataId);
+    }
+catch(e){
+    res.status(500).send(e);
+}
+})
+
+app.delete('/:id', async (req,res)=>{
+    try{
+        const _id = req.params.id
+        const GetDataId = await SongColl.findByIdAndDelete({_id});
+        res.status(200).send(GetDataId);
+    }
+catch(e){
+    res.status(500).send(e);
+}
+})
+
 app.listen(port,()=>{
     console.log(`Server listening at port ${port}`);
 })
